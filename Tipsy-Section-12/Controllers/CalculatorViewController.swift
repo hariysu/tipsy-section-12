09022aed5808a2226d276549a0d4e882dfea3016
Abdurahman AYDIN <hariysu@gmail.com>
2023-10-05 18:11:44 +0300

    Creates IBActions and IBOutlets for calculator and results screen

---
+++ Tipsy-Section-12/Controllers/CalculatorViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tipsy-Section-12
+//
+//  Created by Abdurahman on 5.10.2023.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+}
+
---
+++ Tipsy-Section-12/Controllers/ResultsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ResultsViewController.swift
+//  Tipsy-Section-12
+//
+//  Created by Abdurahman on 5.10.2023.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+}
