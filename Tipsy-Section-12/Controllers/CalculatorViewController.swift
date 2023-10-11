663d91904db176157a2181e35a53d6cc21b6bede
Abdurahman AYDIN <hariysu@gmail.com>
2023-10-11 14:14:14 +0300

    Shows how many people split the bill between

--- Tipsy-Section-12/Controllers/CalculatorViewController.swift
+++ Tipsy-Section-12/Controllers/CalculatorViewController.swift
@@ -30,10 +30,11 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
-        print(chosenTip!)
+        print(splitNumberLabel.text!)
     }
 }
 
